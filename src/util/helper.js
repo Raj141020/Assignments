68325ef8451480a3e30da8b9d2e3de73355e87a9
Raj141020 <rajnagwanshi1410@gmail.com>
2022-10-14 23:28:38 +0530

    ok

--- src/util/helper.js
+++ src/util/helper.js
@@ -2,7 +2,7 @@ const nameofbatch = "Lithium"
 let today = new Date();
 
 function printDate(){
-    console.log("date is " +today.getDate() + " and month is " +today.getMonth() + " and my batch is lithium and today week 3 day 5 anf our topic is NODEJS module system");
+    console.log("date is " +today.getDate() + " and month is " +today.getMonth() + " and my batch is lithium and today week 3 day 5 and our topic is NODEJS module system");
 
 }
 
