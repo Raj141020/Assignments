b74314773f88a96441c2fa4a3017023987d62c89
Raj141020 <rajnagwanshi1410@gmail.com>
2022-10-14 16:46:31 +0530

    added

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,17 +4,21 @@ const router = express.Router();///test-you
 const xyz = require('../logger')
 //importing external package
 const underscore = require('underscore')
+//My code
+const ques1 = require('../logger/logger');
 
 router.get('/test-me', function (req, res) {
+    console.log("Problem 1 answer is ");
+    ques1.printdetails();
     //Calling the components of a different custom module
-    console.log("Calling my function ",xyz.myFunction())
-    console.log("The value of the constant is ",xyz.myUrl)
+    // console.log("Calling my function ",xyz.myFunction())
+    // console.log("The value of the constant is ",xyz.myUrl)
     //Trying to use an external package called underscore
-    let myArray = ['Akash', 'Pritesh', 'Sabiha']
-    let result = underscore.first(myArray)
-    console.log("The result of underscores examples api is : ", result)
+    // let myArray = ['Razzz', 'Pritesh', 'Sabiha']
+    // let result = underscore.first(myArray)
+    // console.log("The result of underscores examples api is : ", result)
     
-    res.send('My first ever api!')
+    res.send('Welcome to my application my name is Raj Nagwanshi and I am from lithium cohort')
 
     //To be tried what happens if we send multiple response
     //res.send('My second api!')
