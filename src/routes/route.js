567dc0cf8e236e9a596e224052e5d3ebd40b42df
Raj141020 <rajnagwanshi1410@gmail.com>
2022-10-17 16:29:32 +0530

    get api

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,37 +2,134 @@ const express = require('express');
 const abc = require('../introduction/intro')
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    console.log('My batch is', abc.name)
-    abc.printName()
-    logger.welcome()
+// router.get('/test-me', function (req, res) {
+//     console.log('My batch is', abc.name)
+//     abc.printName()
+//     logger.welcome()
 
-    res.send('My second ever api!')
+//     res.send('My second ever api!')
+// });
+
+// router.get('/students', function (req, res){
+//     let students = ['Sabiha', 'Neha', 'Akash']
+//     res.send(students)
+// })
+
+
+
+// router.get('/student-details/:name', function(req, res){
+//     /*
+//     params is an attribute inside request that contains 
+//     dynamic values.
+//     This value comes from the request url in the form of an 
+//     object where key is the variable defined in code 
+//     and value is what is sent in the request
+//     */
+
+//     let requestParams = req.params
+
+//     // JSON strigify function helps to print an entire object
+//     // We can use many ways to print an object in Javascript, JSON stringify is one of them
+//     console.log("This is the request "+ JSON.stringify(requestParams))
+//     let studentName = requestParams.name
+//     console.log('Name of the student is ', studentName)
+    
+//     res.send('Dummy response')
+// })
+
+//Question 1
+
+router.get('/movies', function (req, res){
+    let movies = ['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
+    console.log("Rroblem 1 answer is ----------------------->" +movies)
+    res.send(movies)
+})
+
+//Question 2
+
+// router.get('/movies/:indexNumber', function (req, res){
+
+//     let movies = ['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
+
+//     const index = req.params;
+//     const index_no = index.indexNumber;
+
+//     console.log("Rroblem 2 answer is ----------------------->" +movies[index_no]);
+
+//     res.send(movies[index_no]);
+// });
+
+//Question 3
+
+router.get('/movies/:indexNumber', function (req, res){
+
+    
+    console.log("This is the request " + JSON.stringify(req.params));
+    let movies = ['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
+    
+    const index = req.params;
+    const index_no = index.indexNumber;
+    
+    if(index_no < 0 || index_no > movies.length - 1){
+        res.send("please insert valid index number")
+    } else {
+        res.send("movies[index_no]")
+    }
+
 });
 
-router.get('/students', function (req, res){
-    let students = ['Sabiha', 'Neha', 'Akash']
-    res.send(students)
-})
+//Question 4
 
-router.get('/student-details/:name', function(req, res){
-    /*
-    params is an attribute inside request that contains 
-    dynamic values.
-    This value comes from the request url in the form of an 
-    object where key is the variable defined in code 
-    and value is what is sent in the request
-    */
+router.get('/get/films', function (req, res){
 
-    let requestParams = req.params
+    const films = [ {
+        id: 1,
+        name: 'The Shining'
+       }, {
+        id: 2,
+        name: 'Incendies'
+       }, {
+        id: 3,
+        name: 'Rang de Basanti'
+       }, {
+        id: 4,
+        name: 'Finding Nemo'
+       }];
 
-    // JSON strigify function helps to print an entire object
-    // We can use many ways to print an object in Javascript, JSON stringify is one of them
-    console.log("This is the request "+ JSON.stringify(requestParams))
-    let studentName = requestParams.name
-    console.log('Name of the student is ', studentName)
+       res.send(films);
+    console.log("Rroblem 4 answer is ----------------------->" + JSON.stringify(films));
+});
+
+//Question 5
+
+router.get('/get/films/:filmId', function (req, res){
+    console.log("This is the request " + JSON.stringify(req.params));
+
+    const films = [ {
+        id: 1,
+        name: 'The Shining'
+       }, {
+        id: 2,
+        name: 'Incendies'
+       }, {
+        id: 3,
+        name: 'Rang de Basanti'
+       }, {
+        id: 4,
+        name: 'Finding Nemo'
+       }];
+
+       const id = req.params;
+       const id_index = id.filmId;
+
+       for(let i=0;i<films.length;i++){
+        const film = films[i];
+        if(film.id == id_index){
+            return res.send(film)
+        }
+       }
+       res.send("No movie exists with this id")
+       
     
-    res.send('Dummy response')
-})
-
+});
 module.exports = router;
