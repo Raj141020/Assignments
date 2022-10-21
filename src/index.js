6e625781b2c3aa7a804ef66bc767405d44881c40
Raj141020 <rajnagwanshi1410@gmail.com>
2022-10-21 15:26:47 +0530

    update changes

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb+srv://functionup-cohort:3bMssIUqmBmIqb3e@cluster0.3ccgp0v.mongodb.net/PK-100?retryWrites=true&w=majority"
+mongoose.connect("mongodb+srv://Rajnagwanshi:abhishek1410@cluster0.qlrpwrw.mongodb.net/?retryWrites=true&w=majority"
 , {
    useNewUrlParser: true 
 }
