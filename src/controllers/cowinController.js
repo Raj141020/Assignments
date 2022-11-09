0497f955839158627a7f8ea1a8a9457d20f2b40d
Raj141020 <rajnagwanshi1410@gmail.com>
2022-11-09 09:20:51 +0530

    added

--- src/controllers/cowinController.js
+++ src/controllers/cowinController.js
@@ -78,8 +78,151 @@ let getOtp = async function (req, res) {
     }
 }
 
+//Question 1 
+
+let getlist = async function (req, res) {
+    try {
+        let district_id = req.query.district_id
+        let date = req.query.date
+        console.log(`query params are: ${district_id} ${date}`)
+        var options = {
+            method: "get",
+            url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${district_id}&date=${date}`
+        }
+        let result = await axios(options)
+        console.log(result.data)
+        res.status(200).send({ msg: result.data })
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).send({ msg: err.message })
+    }
+}
+
+//Question 2 // Part 1
+
+let getlondanweather = async function (req, res) {
+    try {
+        let appid = req.query.appid
+        console.log(`query params are: ${appid}`)
+        var options = {
+            method: "get",
+            url: `http://api.openweathermap.org/data/2.5/weather?q=London&appid=${appid}`
+        }
+        let result = await axios(options)
+        console.log(result.data)
+        res.status(200).send({ msg: result.data })
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).send({ msg: err.message })
+    }
+}
+
+//Question 2 // Part 2
+
+let getlondanweathertemponly = async function (req, res) {
+    try {
+        let appid = req.query.appid
+        console.log(`query params are: ${appid}`)
+        var options = {
+            method: "get",
+            url: `http://api.openweathermap.org/data/2.5/weather?q=London&appid=${appid}`
+        }
+        let result = await axios(options)
+        console.log(result.data.main.temp)
+        res.status(200).send({ msg: result.data.main.temp })
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).send({ msg: err.message })
+    }
+}
+
+//Question 2 // Part 3
+
+let getweathersorted = async function (req, res) {
+    try {
+        let appid = req.query.appid
+        let cities = ["Bengaluru","Mumbai", "Delhi", "Kolkata", "Chennai", "London", "Moscow"]
+        let arr = []
+        for(let i=0;i<cities.length;i++){
+            var options = {
+            method: "get",
+            url: `http://api.openweathermap.org/data/2.5/weather?q=${cities[i]}&appid=${appid}`
+        }
+
+        let result = await axios(options)
+        console.log("Data is -------------------->")
+        console.log(result.data)
+        arr.push({city:cities[i],temp:result.data.main.temp})
+        console.log("UnSorted answer is -------------------->")
+        console.log(arr)
+        }
+        arr.sort((a,b)=>a.temp - b.temp)
+        console.log("Sorted answer is -------------------->")
+        console.log(arr)
+        res.status(200).send({ msg: arr })
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).send({ msg: err.message })
+    }
+}
+
+//Question 3 Part 1
+
+let memes = async function (req, res) {
+    try {
+        var options = {
+            method: "get",
+            url: `https://api.imgflip.com/get_memes`
+            
+        }
+        let result = await axios(options)
+        console.log(result.data)
+        res.status(200).send({ msg: result.data })
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).send({ msg: err.message })
+    }
+}
+
+//Question 3 Part 2
+
+let getmemes = async function (req, res) {
+    try {
+        const template_id = req.query.template_id
+        const text0 = req.query.text0
+        const text1 = req.query.text1
+        const username  = req.query.username
+        const password  = req.query.password
+        console.log(`query params are: ${template_id} ,${text0}, ${text1}, ${username}, ${password}`)
+        var options = {
+            method: "post",
+            url: `https://api.imgflip.com/caption_image?template_id=${template_id}&test0=${text0}&test1=${text1}&username=${username}&password=${password}`
+
+        }
+        let result = await axios(options)
+        console.log(result.data)
+        res.status(200).send({ msg: result.data })
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).send({ msg: err.message })
+    }
+}
+
+
 
 module.exports.getStates = getStates
 module.exports.getDistricts = getDistricts
 module.exports.getByPin = getByPin
 module.exports.getOtp = getOtp
+module.exports.getlist =getlist
+module.exports.getlondanweather =getlondanweather
+module.exports.getlondanweathertemponly =getlondanweathertemponly
+module.exports.getweathersorted =getweathersorted
+module.exports.getmemes =getmemes
+module.exports.memes =memes
--- src/routes/route.js
+++ src/routes/route.js
@@ -10,12 +10,39 @@ router.get("/test-me", function (req, res) {
 
 
 router.get("/cowin/states", CowinController.getStates)
+
 router.get("/cowin/districtsInState/:stateId", CowinController.getDistricts)
+
 router.get("/cowin/getByPin", CowinController.getByPin)
 
 router.post("/cowin/getOtp", CowinController.getOtp)
 
-// WRITE A GET API TO GET THE LIST OF ALL THE "vaccination sessions by district id" for any given district id and for any given date
+//Key is 8bfe752e8fd0f8f1a1ceba98e24a0bd9
+
+/*Question 1. WRITE A GET API TO GET THE LIST OF ALL THE "vaccination sessions by district id" for any given district id and for any given date */
+
+router.get("/cowin/vaccinationsession",CowinController.getlist)
+
+/*Question 2. Part 1 --> get weather of London*/
+
+router.get("/weather/London",CowinController.getlondanweather)
+
+/*Question 2. Part 2 --> then change the above to get the temperature only( of London)*/
+
+router.get("/weather/London/temponly",CowinController.getlondanweathertemponly)
+
+/*Question 2. Part 3 --> Sort the cities  ["Bengaluru","Mumbai", "Delhi", "Kolkata", "Chennai", "London", "Moscow"] in order of their increasing temperature*/
+
+router.get("/weather/London/sorted",CowinController.getweathersorted)
+
+/* Question 3 Part 1*/
+
+router.get("/memes",CowinController.memes)
+
+/* Question 3 Part 2*/
+
+router.post("/memes",CowinController.getmemes)
+
 
 
 
